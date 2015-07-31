c40fea1b6d6b7ec2ce81a2682084ccbdc5df9931
hknCanaan <hkncanaan@gmail.com>
2015-07-31 13:13:35 +0800

    Re-Start

---
+++ js/bodyCheck.js
@@ -0,0 +1,16 @@
+function init(canvasID){
+	var canvas = {};
+	canvas.ID = canvasID;
+	initResourcesSection(canvas);
+}
+
+function intitAdmiralInfoSection(canvas)
+{
+	
+}
+
+function initResourcesSection(canvas)
+{
+	
+	
+}
