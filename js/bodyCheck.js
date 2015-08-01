cec0cacb025c602b41d809c7603d610ad6a3b5fa
hknCanaan <hkncanaan@gmail.com>
2015-08-01 13:27:18 +0800

    Update bodyCheck.js

--- js/bodyCheck.js
+++ js/bodyCheck.js
@@ -1,6 +1,12 @@
 function init(canvasID){
 	var canvas = {};
 	canvas.ID = canvasID;
+	var canvas = document.getElementById(canvas.ID);
+      	var context = canvas.getContext('2d');
+	
+	context.stroke = "black"; 
+	ctx.lineWidth = 1;
+	context.strokeRect(0, 0, 100, 100);
 	initResourcesSection(canvas);
 }
 
